68a595ca4c069c0bff90466687a40116c505af07
József Széplaki <szeplaki.jozsef97@gmail.com>
2024-12-07 12:34:58 +0100

    The welcome page is updated, now the pictures change over time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import WelcomePage from "./pages/WelcomePage";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import PalyazatokPage from "./pages/PalyazatokPage";
@@ -8,11 +9,20 @@ import PalyazatokPage from "./pages/PalyazatokPage";
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/kapcsolatok" element={<ContactPage />} />
-        <Route path="/palyazatok" element={<PalyazatokPage />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/main" element={<>
+          <Navbar />
+          <HomePage />
+        </>} />
+        <Route path="/kapcsolatok" element={<>
+          <Navbar />
+          <ContactPage />
+        </>} />
+        <Route path="/palyazatok" element={<>
+          <Navbar />
+          <PalyazatokPage />
+        </>} />
       </Routes>
     </Router>
   );
