fd83eef7971de0fe2b0b28663dace9fc34b49de7
Jozsef Szeplaki <szeplaki.jozsef97@gmail.com>
2024-12-12 16:43:48 +0100

    Just one letter changed, for the working Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import "./Navbar.css";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <div className="nav-icon">☰</div>
       <div className="nav-links">
-        <NavLink to="/main" className="nav-link">Főoldal</NavLink>
-        <NavLink to="/kapcsolatok" className="nav-link">Kapcsolatok</NavLink>
-        <NavLink to="/palyazatok" className="nav-link">Pályázatok</NavLink>
+        <NavLink to="/main" className="nav-link">
+          Főoldal
+        </NavLink>
+        <NavLink to="/kapcsolatok" className="nav-link">
+          Kapcsolatok
+        </NavLink>
+        <NavLink to="/palyazatok" className="nav-link">
+          Pályázatok
+        </NavLink>
       </div>
     </div>
   );
