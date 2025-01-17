3972a450f8efbd2575a8bce97ccdb8f116f3d65f
J0skaa <szeplaki.jozsef97@gmail.com>
2025-01-17 20:30:04 +0100

    The backend is tested with Postman, every response is working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
-require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const mongoose = require("mongoose");
+const Post = require("./models/Post");
 const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
 MONGO_URI =
   "mongodb+srv://j0ska:pilisszantose101@jozsef-cluster.1tx92.mongodb.net/?retryWrites=true&w=majority&appName=jozsef-cluster";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -17,13 +21,34 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error", err));
 
-// Test route
-
-app.get("/", (req, res) => {
-  res.send("Backend test");
+app.get("/api/posts", async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }).limit(3);
+    res.json(posts);
+  } catch (error) {
+    console.error("Error fetching posts", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
+
+app.post("/api/posts", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(400).json({ message: "Title and content required" });
+    }
+
+    const newPost = new Post({ title, content });
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).json({ message: "Internal server Error" });
+  }
 });
